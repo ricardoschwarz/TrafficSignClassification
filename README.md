1a90becf034e8865c249ef380cd598bb9476df1f
ricardoschwarz <schwarz.ricardo.ii@gmail.com>
2019-08-12 23:52:41 +0200

    changed dataset to data; added data to .gitignore; added alexnet implementation to training.py; added keras trainingset generator to training.py

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # SpeedLimit_DL
 A deep learning project to estimate the current speedlimit from a front camera video feed.
+
+AlexNet is trained from the German Trafic Sign Dataset from https://sid.erda.dk/public/archives/daaeac0d7ce1152aea9b61d9f1e19370/published-archive.html
+
+The dataset structure should be:
+
+./data/
+    -- train/
+        -- 00000/
+            -- 00000_00000.ppm
+            -- 00000_00001.ppm
+            -- ...
+        -- ...
+    ...
--- training.py
+++ training.py
@@ -1,3 +1,96 @@
 # script to train a cnn (probably AlexNet) with the German Trafic Sign Dataset from https://sid.erda.dk/public/archives/daaeac0d7ce1152aea9b61d9f1e19370/published-archive.html
 
 # TODO
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout, Flatten, Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+import numpy as np
+np.random.seed(1000)
+
+# create a data generator
+datagen = keras.preprocessing.image.ImageDataGenerator()
+
+# load and iterate training dataset
+train_it = datagen.flow_from_directory('dataset/train/', class_mode='binary', batch_size=64)
+
+# (3) Create a sequential model
+model = Sequential()
+
+# 1st Convolutional Layer
+model.add(Conv2D(filters=96, input_shape=(224,224,3), kernel_size=(11,11),\
+ strides=(4,4), padding='valid'))
+model.add(Activation('relu'))
+# Pooling 
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+# Batch Normalisation before passing it to the next layer
+model.add(BatchNormalization())
+
+# 2nd Convolutional Layer
+model.add(Conv2D(filters=256, kernel_size=(11,11), strides=(1,1), padding='valid'))
+model.add(Activation('relu'))
+# Pooling
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+# Batch Normalisation
+model.add(BatchNormalization())
+
+# 3rd Convolutional Layer
+model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid'))
+model.add(Activation('relu'))
+# Batch Normalisation
+model.add(BatchNormalization())
+
+# 4th Convolutional Layer
+model.add(Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), padding='valid'))
+model.add(Activation('relu'))
+# Batch Normalisation
+model.add(BatchNormalization())
+
+# 5th Convolutional Layer
+model.add(Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), padding='valid'))
+model.add(Activation('relu'))
+# Pooling
+model.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), padding='valid'))
+# Batch Normalisation
+model.add(BatchNormalization())
+
+# Passing it to a dense layer
+model.add(Flatten())
+# 1st Dense Layer
+model.add(Dense(4096, input_shape=(224*224*3,)))
+model.add(Activation('relu'))
+# Add Dropout to prevent overfitting
+model.add(Dropout(0.4))
+# Batch Normalisation
+model.add(BatchNormalization())
+
+# 2nd Dense Layer
+model.add(Dense(4096))
+model.add(Activation('relu'))
+# Add Dropout
+model.add(Dropout(0.4))
+# Batch Normalisation
+model.add(BatchNormalization())
+
+# 3rd Dense Layer
+model.add(Dense(1000))
+model.add(Activation('relu'))
+# Add Dropout
+model.add(Dropout(0.4))
+# Batch Normalisation
+model.add(BatchNormalization())
+
+# Output Layer
+model.add(Dense(17))
+model.add(Activation('softmax'))
+
+model.summary()
+
+# (4) Compile 
+model.compile(loss='categorical_crossentropy', optimizer='adam',\
+ metrics=['accuracy'])
+
+# (5) Train
+model.fit(train_it, train_it, batch_size=64, epochs=1, verbose=1, \
+validation_split=0.2, shuffle=True)
