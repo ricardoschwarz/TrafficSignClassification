95e690e8c8d6e181198ec6210f0b11bf4d01b0d7
ricardoschwarz <schwarz.ricardo.ii@gmail.com>
2019-08-12 22:22:13 +0200

    adding training.py

---
+++ training.py
@@ -0,0 +1,3 @@
+# script to train a cnn (probably AlexNet) with the German Trafic Sign Dataset from https://sid.erda.dk/public/archives/daaeac0d7ce1152aea9b61d9f1e19370/published-archive.html
+
+# TODO
