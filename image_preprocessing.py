750690f2a26cde1dc06540c238a3d7e4e2a266e3
Ricardo Schwarz <schwarz.ricardo.ii@gmail.com>
2019-08-17 14:53:04 +0200

    +added image_preprocessing.py; +resizing of training_images is now done via image_preprocessing.py; +added docstring to show_images; +refactored loading of training data into method load_training_data; +introduced test set, this is split from training_data; +added model evaluation;

---
+++ image_preprocessing.py
@@ -0,0 +1,31 @@
+import os
+from keras.preprocessing.image import load_img
+ 
+def resize_images(image_dir):
+    """loads, resizes and saves all training images to fixed size
+	WARNING: training images will be overriden
+
+	Parameters:
+	image_dir(str): directory of images
+    """ 
+    
+    filenames = []
+
+    for subdir, dirs, files in os.walk(image_dir):
+        for file in files: 
+            if file.endswith(".ppm"):
+                filenames.append(os.path.join(subdir, file))
+
+    import PIL
+    i = 0
+    for _file in filenames:
+        img = load_img(_file)  # this is a PIL image
+        img = img.resize((50, 50), PIL.Image.ANTIALIAS)
+        img.save(_file)
+        i+=1
+        if i % 250 == 0:
+            print("%d images resized" % i)
+        
+
+image_folder = "data/"
+resize_images(image_folder)
--- training.py
+++ training.py
@@ -1,11 +1,21 @@
 # ----------
 # script to train a simple cnn (later maybe AlexNet) with the German Trafic Sign Dataset from https://sid.erda.dk/public/archives/daaeac0d7ce1152aea9b61d9f1e19370/published-archive.html
+#
+# run image_preprocessing.py once before running this script
 # ----------
 
-# useful methods
+## useful methods
 
 import matplotlib.pyplot as plt
 def show_images(images, labels, classes):
+	"""shows a diagram from input images with corresponding class labels
+
+	Parameters:
+	images(PIL image): images to display
+	labels(str): array of labels
+	classes(str): array of all possible classes
+
+	"""    
 	plt.figure(figsize=(10,10))
 	for i in range(25):
 		plt.subplot(5,5,i+1)
@@ -17,42 +27,36 @@ def show_images(images, labels, classes):
 					
 	plt.show()
 
+def load_training_data(image_dir):
+	"""Loads training images.
+	IMPORTANT: make sure all images are resized properly (50x50x3)
 
-# main
+	Parameters:
+	image_dir(str): directory of training images
 
-if __name__ == "__main__":
+	Return:
+	(dataset, y_train): tuple of dataset (shape: number_img, image_width, image_height, img_depth) and corresponding labels
+	"""
+	train_files = []
+	y_train = []
 
-	# load the dataset
 	import os
-	train_folder = "data/train"
-
-	filenames = []
-	for subdir, dirs, files in os.walk(train_folder):
+	for subdir, dirs, files in os.walk(image_dir):
 		for file in files: 
 			if file.endswith(".ppm"):
-				filenames.append(os.path.join(subdir, file))
+				file_name = os.path.join(subdir, file)
+				train_files.append(file_name)
+				y_train.append(int(file_name[11:16]))
 
-	print("Working with {0} images".format(len(filenames)))
+	print("Working with {0} training images".format(len(train_files)))
 
 	from scipy import ndimage
 	from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 
-	train_files = []
-	y_train = []
-	i=0
-	for _file in filenames:
-		train_files.append(_file)
-		label_in_file = _file.find("_")
-		y_train.append(int(_file[11:16]))
-		
-	print("Files in train_files: %d" % len(train_files))
-
 	# Original Dimensions
 	image_height = 50
 	image_width = 50
-
 	channels = 3
-	nb_classes = 1
 
 	import numpy as np
 	dataset = np.ndarray(shape=(len(train_files), image_height, image_width, channels),
@@ -62,10 +66,8 @@ if __name__ == "__main__":
 	i = 0
 	for _file in train_files:
 		img = load_img(_file)  # this is a PIL image
-		img = img.resize((image_height,image_width), PIL.Image.ANTIALIAS)
 		# Convert to Numpy Array
 		x = img_to_array(img)  
-		# x = x.reshape((30, 29, 3))
 		# Normalize
 		x = x / 255.0
 		dataset[i] = x
@@ -74,11 +76,22 @@ if __name__ == "__main__":
 			print("%d images to array" % i)
 	print("All images to array!")
 
-	from sklearn.model_selection import train_test_split
+	return (dataset, y_train)
+  
+
+## main
+
+if __name__ == "__main__":
+
+	# load the dataset
+	train_image_dir = "data/train"
+	X_data, y_data = load_training_data(train_image_dir)
 
 	#Splitting 
-	X_train, X_val, y_train, y_val = train_test_split(dataset, y_train, test_size=0.2, random_state=33)
-	print("Train set size: {0}, Val set size: {1}, Test set size: NOT YET".format(len(X_train), len(X_val)))
+	from sklearn.model_selection import train_test_split
+	X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=15)
+	X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=33)
+	print("Train set size: {0}, Val set size: {1}, Test set size: {2}".format(len(X_train), len(X_val), len(X_test)))
 
 	from tensorflow.python import keras
 	model = keras.Sequential([
@@ -91,9 +104,9 @@ if __name__ == "__main__":
 			loss='sparse_categorical_crossentropy',
 			metrics=['accuracy'])
 
-	model.fit(X_train, y_train, epochs=5, validation_data=(X_val, y_val))
+	model.fit(X_train, y_train, epochs=10, validation_data=(X_val, y_val))
 
-	# test_loss, test_acc = model.evaluate(X_test, y_test)
+	test_loss, test_acc = model.evaluate(X_test, y_test)
 
 	classes = { 
 		0 : "Limit 20", 
